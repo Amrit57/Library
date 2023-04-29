04695450082919d819427a05c368535d8560e5c0
Amrit57 <fenpadorje7@gmail.com>
2023-04-29 16:31:20 +0545

    Added functions to delete new books

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 
+//Accesing values From Form.
 const tbody = document.querySelector('#table-body')
 const bookTitle = document.querySelector('#book-title')
 const bookAuthor = document.querySelector('#author-name')
@@ -7,32 +8,41 @@ const bookPages = document.querySelector('#number')
 const selectOption = document.querySelector('#select')
 const addBookBtn = document.querySelector('#add-book')
 
+//eventListener to change status in existing items in libary
 let statusBtns = document.querySelectorAll('.status')
 statusBtns.forEach((btn) => {
-    btn.addEventListener('click', function changeStatus(item) {
+    btn.addEventListener('click', function (item) {
         let text = item.target;
         if (text.textContent === 'Unread') {
-            text.classList.add('green-bg')
             text.classList.add('status')
             text.textContent = 'Read';
         } else if (text.textContent === 'Read') {
             text.textContent = 'Unread';
-            text.classList.remove('green-bg')
         }
-        console.log(item.target.textContent)
     })
 })
 
+//eventListener to delete rows in existing items in libary
+let deleteBtns = document.querySelectorAll('.delete')
+deleteBtns.forEach((btn) => {
+    btn.addEventListener('click', function () {
+        btn.parentElement.parentElement.remove();
+    })
+})
+
+//event to add books to the table
 addBookBtn.addEventListener('click', addToLibrary)
 
-
+//values to create new row,books data
 let newRow;
 let newTitle;
 let newAuthor;
 let newBookPages;
 let newBookStatus;
 let newDeleteBtn;
+let deleteBtn;
 
+//constructor to add books
 function Book(title, author, pages, status) {
     this.title = title,
         this.author = author,
@@ -40,6 +50,7 @@ function Book(title, author, pages, status) {
         this.status = status;
 }
 
+//function that adds books to library
 function addToLibrary() {
     let book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, selectOption.value);
     newRow = document.createElement('tr')
@@ -57,19 +68,9 @@ function addToLibrary() {
     newBookPages.classList.add('tdata')
 
     newBookStatus.classList.add('tdata')
-    newStatusBTn.classList.add('status')
-    newDeleteBtn.classList.add('delete')
     newBtnTd.classList.add('tdata')
-    newStatusBTn.addEventListener('click', function changeStatus(item) {
-        let text = item.target;
-        if (text.textContent === 'Unread') {
-            text.classList.add('status')
-            text.textContent = 'Read';
-        } else if (text.textContent === 'Read') {
-            text.textContent = 'Unread';
-        }
-        console.log(item.target.textContent)
-    })
+    newStatusBTn.classList.add('status')
+    newDeleteBtn.classList.add('remove')
 
     newRow.appendChild(newTitle)
     newRow.appendChild(newAuthor)
@@ -81,14 +82,35 @@ function addToLibrary() {
     newRow.appendChild(newBtnTd)
 
     tbody.appendChild(newRow)
-    myLibrary.push(book);
+    myLibrary.push(book); //pushing new books to myLibrary Array
 
     displayBooks()
     resetForm()
-
+    deleteRow()
+    newStatusBTn.addEventListener('click', changeStatus) // event to change status in newly created books
 }
 
+function changeStatus(item) {
+    let text = item.target;
+    if (text.textContent === 'Unread') {
+        return text.textContent = 'Read';
+    } else if (text.textContent === 'Read') {
+        return text.textContent = 'Unread';
+    }
+}
+ //adding event and function to delete rows in newly created books
+function deleteRow() {
+    deleteBtn = document.querySelectorAll('.remove')
+    deleteBtn.forEach((item) => {
 
+        item.addEventListener('click', function () {
+            item.parentElement.parentElement.remove();
+
+        })
+    })
+}
+
+//function to display books in table
 function displayBooks() {
 
     for (i = 0; i < myLibrary.length; i++) {
@@ -98,8 +120,9 @@ function displayBooks() {
         newStatusBTn.textContent = myLibrary[i].status;
         newDeleteBtn.textContent = 'X';
     }
-
 }
+
+//function which resets form input values.
 function resetForm() {
     bookTitle.value = '';
     bookAuthor.value = '';
